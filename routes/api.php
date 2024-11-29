4323b1cdb9bd8fce46c4d04e9abde38c8c7e99d8
rebecabernal <rebecabernalmesa@gmail.com>
2024-11-29 13:56:07 +0100

    fix: routes naming

--- routes/api.php
+++ routes/api.php
@@ -2,15 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\OfferController;
+use App\Http\Controllers\Api\FeedbackController;
 
-Route::get('/offers', [JobController::class, 'index'])->name('apiindex');
-Route::post('/offers', [JobController::class, 'store'])->name('apistore');
-Route::get('/offers/{id}', [JobController::class, 'show'])->name('apishow');
-Route::put('/offers/{id}', [JobController::class, 'update'])->name('apiupdate');
-Route::delete('/offers/{id}', [JobController::class, 'destroy'])->name('apidestroy');
+Route::get('/offers', [OfferController::class, 'index'])->name('apiindex');
+Route::post('/offers', [OfferController::class, 'store'])->name('apistore');
+Route::get('/offers/{id}', [OfferController::class, 'show'])->name('apishow');
+Route::put('/offers/{id}', [OfferController::class, 'update'])->name('apiupdate');
+Route::delete('/offers/{id}', [OfferController::class, 'destroy'])->name('apidestroy');
 
-Route::get('/offers/{jobId}/comments', [FeedbackController::class, 'index'])->name('apiindexComments');
-Route::post('/offers/{jobId}/comments', [FeedbackController::class, 'store'])->name('apistoreComments');
-Route::get('/offers/{jobId}/comments/{commentId}', [FeedbackController::class, 'show'])->name('apishowComments');
-Route::put('/offers/{jobId}/comments/{commentId}', [FeedbackController::class, 'update'])->name('apiupdateComments');
-Route::delete('/offers/{jobId}/comments/{commentId}', [FeedbackController::class, 'destroy'])->name('apidestroyComments');
+Route::get('/offers/{offerId}/comments', [FeedbackController::class, 'index'])->name('apiindexComments');
+Route::post('/offers/{offerId}/comments', [FeedbackController::class, 'store'])->name('apistoreComments');
+Route::get('/offers/{offerId}/comments/{commentId}', [FeedbackController::class, 'show'])->name('apishowComments');
+Route::put('/offers/{offerId}/comments/{commentId}', [FeedbackController::class, 'update'])->name('apiupdateComments');
+Route::delete('/offers/{offerId}/comments/{commentId}', [FeedbackController::class, 'destroy'])->name('apidestroyComments');
