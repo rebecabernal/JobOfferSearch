81fb9948ad29066b1ad70d0a56bb6703de41952f
rebecabernal <rebecabernalmesa@gmail.com>
2024-11-29 18:07:08 +0100

    fix:routes naming

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FeedbackController;
 use App\Http\Controllers\Api\OfferController;
-use App\Http\Controllers\Api\FeedbackController;
 
 Route::get('/offers', [OfferController::class, 'index'])->name('apiindex');
 Route::post('/offers', [OfferController::class, 'store'])->name('apistore');
@@ -11,8 +11,8 @@ Route::get('/offers/{id}', [OfferController::class, 'show'])->name('apishow');
 Route::put('/offers/{id}', [OfferController::class, 'update'])->name('apiupdate');
 Route::delete('/offers/{id}', [OfferController::class, 'destroy'])->name('apidestroy');
 
-Route::get('/offers/{offerId}/comments', [FeedbackController::class, 'index'])->name('apiindexComments');
-Route::post('/offers/{offerId}/comments', [FeedbackController::class, 'store'])->name('apistoreComments');
-Route::get('/offers/{offerId}/comments/{commentId}', [FeedbackController::class, 'show'])->name('apishowComments');
-Route::put('/offers/{offerId}/comments/{commentId}', [FeedbackController::class, 'update'])->name('apiupdateComments');
-Route::delete('/offers/{offerId}/comments/{commentId}', [FeedbackController::class, 'destroy'])->name('apidestroyComments');
+Route::get('/offers/{offerId}/news', [FeedbackController::class, 'index'])->name('apiindexNews');
+Route::post('/offers/{offerId}/news', [FeedbackController::class, 'store'])->name('apistoreNews');
+Route::get('/offers/{offerId}/news/{newsID}', [FeedbackController::class, 'show'])->name('apishowNews');
+Route::put('/offers/{offerId}/news/{newsID}', [FeedbackController::class, 'update'])->name('apiupdateNews');
+Route::delete('/offers/{offerId}/news/{newsId}', [FeedbackController::class, 'destroy'])->name('apidestroyNews');
