03556ce3bcf8fcad65cb66e9a75ed40326024a13
rebecabernal <rebecabernalmesa@gmail.com>
2024-12-01 04:49:34 +0100

    fix: offers migration table description format

--- database/migrations/2024_11_26_104021_create_offers_table.php
+++ database/migrations/2024_11_26_104021_create_offers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('offers', function (Blueprint $table) {
             $table->id();
             $table->string('enterprise');
-            $table->string('description');
+            $table->text('description');
             $table->boolean('status');
             $table->timestamps();
         });
