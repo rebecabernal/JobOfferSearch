d30852874d227475d9376937e6fa0bbea84326f7
rebecabernal <rebecabernalmesa@gmail.com>
2024-12-01 05:07:53 +0100

    fix: homeblade info & naming

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,8 @@
         <table class="table">
             <th>
                 <tr>
+                    <th scope="col">Title</th>
                     <th scope="col">Enterprise</th>
-                    <th scope="col">Workspace</th>
                     <th scope="col">Description</th>
                     <th scope="col">Status</th>
                     <th scope="col">Applied</th>
@@ -16,12 +16,12 @@
             <tb>
             @foreach ($offers as $offer)
                     <tr>
+                        <td>{{$offer->title}}</td>
                         <td>{{$offer->enterprise}}</td>
-                        <td>{{$offer->workspace}}</td>
                         <td>{{$offer->description}}</td>
                         <td>{{$offer->status}}</td>
                         <td>{{$offer->created_at}}</td>
-                        <td>{{$offer->update}}</td>
+                        <td>{{$offer->updated_at}}</td>
                     </tr>
                 @endforeach
             </tb>
